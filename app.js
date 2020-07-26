7ecc18f806e1540ec28a9138aa48702b25ad55ef
beata <beata@emc2wave.com>
2020-07-26 15:15:36 -0700

    add event listener to toggleBtn with if

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const toggleBtn = document.querySelector('.sidebar-toggle');
 const closeBtn = document.querySelector('.closeBtn');
-const sidebar = document.querySelector('.ssidebar');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  if (sidebar.classList.contains('show-sidebar')) {
+    sidebar.classList.remove('show-sidebar');
+  } else {
+    sidebar.classList.add('show-sidebar');
+  }
+});
